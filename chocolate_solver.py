a3fe20c6a7da0467ef69cf4c3c6a7c8877eff084
Vanya Eccles <ecclesva@tcd.ie>
2021-07-22 12:24:50 +0100

    Implemented a naive monte carlo heuristic

--- chocolate_solver.py
+++ chocolate_solver.py
@@ -1,4 +1,7 @@
 from collections import defaultdict
+import math
+import random
+import copy
 
 class ChocolateSolver():
 
@@ -28,6 +31,12 @@ class ChocolateSolver():
             print("No solution available: not enough chocolate to satisfy requested amounts, all children will go hungry.")
             return -1
 
+
+        if self.verbose:
+            print()
+            print(f"Checking for perfect matches")
+            print()
+
         # Step 2:
         # Remove any perfect matches
         # Start by adding lengths to a dictionary of lengths arrays - O(m)
@@ -43,126 +52,39 @@ class ChocolateSolver():
             else:
                 new_desired_lengths.append(length)
 
+        # Rebuild the lists
         desired_lengths = new_desired_lengths
         bar_lengths = []
         for item in bar_length_dict.keys():
             for amount in range(bar_length_dict[item]):
                 bar_lengths.append(item)
 
+        if self.verbose:
+            print()
+            print(f"Checking for additive combinations")
+            print()
+
         # Step 3:
         # Test for additive combinations - O(n*m*k)
         desired_lengths, bar_lengths = self.checkAdditiveCombinations(desired_lengths, bar_lengths)
 
-        # Step 4:
-        # Greedily add small values to create desired lengths - O(n*m)
-        bar_lengths.sort()
-        desired_lengths.sort()
-        new_desired_lengths = []
-        early_stop = False
-        for desired_length in desired_lengths:
-            additive_length = 0
-            component_lengths = []
-            length_acquired = False
-            for index,candidate_length in enumerate(bar_lengths):
-                # If values are becoming larger then proceed to next heuristic (greedy cutting)
-                if candidate_length > desired_length:
-                    early_stop = True
-                    break
-
-                new_length = additive_length + candidate_length
-                if new_length <= desired_length:
-                    additive_length += candidate_length
-                    component_lengths.append(candidate_length)
-                    bar_lengths.pop(index)
-                else:
-                    length_surplus = new_length - desired_length
-                    desired_portion = candidate_length - length_surplus
-                    bar_lengths[index] = length_surplus
-                    self.addAction(f"Cut {candidate_length} to {length_surplus}")
-                    component_lengths.append(desired_portion)
-                    additive_length += desired_portion
-                if additive_length == desired_length:
-                    self.addAction(f"Distribute {component_lengths} to child that wants {desired_length}")
-                    remove_list.append(desired_length)
-                    lengthAcquired = True
-                    break
-            # Add any rejects for further processing
-            if length_acquired == False:
-                new_desired_lengths.append(desired_length)
-            # Proceed to next heuristic
-            if early_stop:
-                break
-
-        # Test again for additive combinations - O(n*m*k)
-        desired_lengths, bar_lengths = self.checkAdditiveCombinations(desired_lengths, bar_lengths)
-
-        if len(desired_lengths) == 0:
-            self.addAction("Returned after step 4")
-            return self.cuts_made
-
-        # Step 5:
-        # Greedily make cuts - O(n*m)
-        # Find first that is larger and cut to size
-        remove_list = []
-        for length in desired_lengths:
-            for index,candidate_length in enumerate(bar_lengths):
-                if candidate_length > length:
-                    self.addAction(f"Cut {candidate_length} to {length} for child that wants {length}")
-                    self.cuts_made += 1
-                    remainder_length = candidate_length - length
-                    bar_lengths[index] = remainder_length
-                    remove_list.append(length)
-                    break
-
-        for element in remove_list:
-            desired_lengths.remove(element)
-
-        # Test again for additive combinations - O(n*m*k)
-        desired_lengths, bar_lengths = self.checkAdditiveCombinations(desired_lengths, bar_lengths)
+        if self.verbose:
+            print()
+            print(f"Beginning naive Monte Carlo heuristic")
+            print()
 
 
-        if len(desired_lengths) == 0:
-            self.addAction("Returned after step 5")
-            return self.cuts_made
+        # Work in progress: Very naive Monte Carlo heuristic
+        num_sample_iterations = 20
+        self.cuts_made, optimal_actions = self.randomSampling(num_sample_iterations, bar_lengths, desired_lengths)
 
 
+        return self.cuts_made
 
-        # Step 5:
-        # Cut any remaining pieces larger than 1 into pieces no larger than 2
-        new_bar_lengths = []
-        amount_needed = len(desired_lengths)
-        small_piece_sum = 0
-        for length in bar_lengths:
-            even_flag = False
-            while length > 2:
-                # Alternate between 1,2
-                even_flag = not even_flag
-                if even_flag:
-                    cut_amount = 1
-                else:
-                    cut_amount = 2
-                remainder = length - cut_amount
-                self.addAction(f"Cut {length} by {cut_amount} into {remainder}")
-                length -= cut_amount
-                self.cuts_made +=1
-                new_bar_lengths.append(cut_amount)
-                small_piece_sum += cut_amount
-            # No more cuts needed
-            if small_piece_sum >= amount_needed:
-                break
-
-        # Test again for additive combinations - O(n*m*k)
-        desired_lengths, bar_lengths = self.checkAdditiveCombinations(desired_lengths, new_bar_lengths)
-
-        if len(desired_lengths) == 0:
-            self.addAction("Returned after step 5")
-            return self.cuts_made
-
-
-        return self.cuts_made + 1
 
 
     def addAction(self, description):
+        # Prints a string to the console if running in verbose mode
         if self.verbose:
             print(description)
 
@@ -192,11 +114,10 @@ class ChocolateSolver():
 
         return desired_lengths, available_lengths
 
-
     def findSubSetSumDP(self, candidate_lengths, desired_length):
         """ Searches for a given length among possible subsets of a list
 
-            Time Complexity - O(desired_length * m)
+            Time Complexity - O(desired_length * m) -> pseudopolynomial
             Space Complexity - O(desired_length * m)
 
             Parameters
@@ -216,11 +137,9 @@ class ChocolateSolver():
         # Table of parent pointers
         parent = ([[None for i in range(desired_length + 1)] for i in range(n + 1)])
 
-        # Build the table
-        #for i in range(n + 1):
-        #    sum[i][0] = True
-        sum[0][0] = True
 
+        # Build the table
+        sum[0][0] = True
         for i in range(1, n + 1):
             for j in range(0, desired_length + 1):
                 sum[i][j] = sum[i-1][j]
@@ -233,10 +152,11 @@ class ChocolateSolver():
         if sum[n][desired_length] != True:
             return False
 
+
         pieceList = []
 
         def reportSubSet(n, k):
-            # Build a trace for the distribution list
+            # Recursively follow the parent table to build the solution subset
             if k == 0:
                 return
             if parent[n][k] == None:
@@ -245,6 +165,141 @@ class ChocolateSolver():
                 reportSubSet(n-1, parent[n][k])
                 pieceList.append(k - parent[n][k])
 
+        # Run the recursive function to retrieve the subset as a list
         reportSubSet(n, desired_length)
 
         return pieceList
+
+
+    def randomSampling(self, n_sample_iterations, candidate_lengths, desired_lengths):
+        """ Performs random samples of different min_cut solutions
+
+            Parameters
+            ----------
+            n_sample_iterations: integer number of samples
+            candidate_lengths: list of ints
+            desired_lengths: list of ints
+
+            Returns
+            -------
+            int: minimum number of cuts to achieve a solution as found from the random sampling
+            list of strings: the actions taken during the best iteration of the random sampling
+        """
+        lowest_cost = math.inf
+        optimal_actions = []
+
+        for i in range(n_sample_iterations):
+
+            candidate_length_copy = copy.deepcopy(candidate_lengths)
+            desired_length_copy = copy.deepcopy(desired_lengths)
+
+            # Sample a random solution
+            current_cost, actions_taken = self.randomSolution(candidate_length_copy, desired_length_copy)
+
+            if self.verbose:
+                print(f"Random solution obtained with cost: {current_cost}")
+                print()
+
+            if current_cost < lowest_cost:
+                lowest_cost = current_cost
+                optimal_actions = actions_taken
+
+        return lowest_cost, optimal_actions
+
+    def randomSolution(self, candidate_lengths, desired_lengths):
+        """ Gets a random solution to a min_cut problem instance
+
+            Parameters
+            ----------
+            candidate_lengths: list of ints
+            desired_lengths: list of ints
+
+            Returns
+            -------
+            int: minimum number of cuts in this solution instance
+            list of strings: list of the actions taken for this sample
+        """
+        sample_cuts = 0
+        # Array for storing the actions taken while running
+        actions_taken = []
+
+        while len(desired_lengths) > 0:
+
+            # Get a random subset from desired_lengths equal to 1/4 of the remaining elements (+ 1 to account for single element list)
+            desired_length_count = len(desired_lengths)
+            sample_count = math.floor(desired_length_count/4) + 1
+            random_desired_lengths = random.sample(desired_lengths, sample_count)
+
+            # Greedily make cuts to fit a desired length - O(n*m)
+            remove_list = []
+            for length in random_desired_lengths:
+                # Find the element that is largest and cut it to size
+                for index,candidate_length in enumerate(candidate_lengths):
+                    if candidate_length > length:
+                        self.addAction(f"Cut {candidate_length} to {length} for child that wants {length}")
+                        sample_cuts += 1
+                        remainder_length = candidate_length - length
+                        candidate_lengths[index] = remainder_length
+                        remove_list.append(length)
+                        break
+
+            # Filter the above solved elements from the desired_lengths
+            for element in remove_list:
+                desired_lengths.remove(element)
+
+            # Test again for additive combinations - O(n*m*k)
+            desired_lengths, candidate_lengths = self.checkAdditiveCombinations(desired_lengths, candidate_lengths)
+
+
+            if len(desired_lengths) == 0:
+                break
+
+
+            # Greedily add small values to create desired lengths - O(n*m)
+            candidate_lengths.sort()
+            desired_lengths.sort()
+            remove_list = []
+            early_stop = False
+            for desired_length in desired_lengths:
+                additive_length = 0
+                component_lengths = []
+                length_acquired = False
+                for index,candidate_length in enumerate(candidate_lengths):
+                    # If values are becoming larger then proceed to next heuristic (greedy cutting)
+                    if candidate_length > desired_length:
+                        early_stop = True
+                        break
+
+                    new_length = additive_length + candidate_length
+                    if new_length <= desired_length:
+                        additive_length += candidate_length
+                        component_lengths.append(candidate_length)
+                        candidate_lengths.pop(index)
+                    else:
+                        length_surplus = new_length - desired_length
+                        desired_portion = candidate_length - length_surplus
+                        candidate_lengths[index] = length_surplus
+                        self.addAction(f"Cut {candidate_length} to {length_surplus}")
+                        sample_cuts += 1
+                        component_lengths.append(desired_portion)
+                        additive_length += desired_portion
+                    if additive_length == desired_length:
+                        self.addAction(f"Distribute {component_lengths} to child that wants {desired_length}")
+                        remove_list.append(desired_length)
+                        lengthAcquired = True
+                        break
+                # Proceed to next heuristic
+                if early_stop:
+                    break
+
+            for element in remove_list:
+                desired_lengths.remove(element)
+
+            # Test again for additive combinations - O(n*m*k)
+            desired_lengths, candidate_lengths = self.checkAdditiveCombinations(desired_lengths, candidate_lengths)
+
+            if len(desired_lengths) == 0:
+                break
+
+
+        return sample_cuts, actions_taken
--- start.py
+++ start.py
@@ -21,7 +21,7 @@ def checkInputFormatting(inputString):
         inputAsArray = [int(x) for x in inputString.rstrip().split(',')]
         return inputAsArray
 
-    except:
+    except ValueError:
         print("Input Error: Please enter in the form: {int1,int2,int3,int7}")
         return -1
 
@@ -32,23 +32,33 @@ def main(is_verbose):
 
         chocolateSolver = ChocolateSolver()
 
-        # Request and validate the bar length inputs
-        barLengthInputMessage = "Please enter the chocolate bar lengths in the form of {length1,length2,lengthN}:"
-        barLengthInput = input(barLengthInputMessage)
-        barLengths = checkInputFormatting(barLengthInput)
+        # Alternatively inputs can be specified in the script
+        # Each of length 100
+        desiredLengths = [5, 9, 33, 41, 42, 19, 9, 20, 12, 30, 29, 25, 40, 2, 28, 31, 29, 36, 5, 48, 20, 20, 24, 9, 2, 46, 6, 48, 49, 34, 43, 2, 10, 27, 27, 11, 16, 9, 25, 28, 12, 21, 47, 2, 49, 39, 35, 28, 16, 43, 20, 26, 5, 42, 47, 6, 12, 42, 22, 11, 3, 37, 6, 40, 3, 30, 15, 2, 33, 11, 41, 40, 33, 28, 7, 25, 6, 40, 19, 9, 46, 9, 17, 25, 28, 4, 17, 28, 39, 16, 1, 46, 39, 32, 45, 28, 28, 41, 3, 32]
+        barLengths = [90, 90, 51, 55, 57, 86, 78, 60, 58, 51, 81, 86, 94, 5, 13, 78, 50, 84, 97, 96, 87, 59, 54, 68, 80, 72, 50, 85, 85, 84, 17, 23, 2, 44, 6, 39, 15, 81, 27, 3, 34, 33, 92, 80, 40, 84, 56, 27, 88, 56, 39, 80, 21, 13, 88, 52, 36, 81, 64, 1, 60, 24, 63, 25, 58, 14, 47, 85, 76, 81, 79, 33, 71, 75, 48, 93, 31, 36, 41, 4, 6, 72, 74, 87, 84, 1, 75, 64, 49, 20, 98, 64, 46, 58, 14, 26, 52, 38, 83, 35]
+
+
+        if 'barLengths' not in locals():
+            # Request and validate the bar length inputs
+            barLengthInputMessage = "Please enter the chocolate bar lengths in the form of {length1,length2,lengthN}:"
+            barLengthInput = input(barLengthInputMessage)
+            barLengths = checkInputFormatting(barLengthInput)
 
         if barLengths == -1:
             continue
 
-        # Request and validate the desired length inputs
-        desiredLengthInputMessage = "Please enter the desired lengths of chocolate in the form of {desiredAmount1,desiredAmount2,desiredAmountN}:"
-        desiredLengthInput = input(desiredLengthInputMessage)
-        desiredLengths = checkInputFormatting(desiredLengthInput)
+        if 'desiredLengths' not in locals():
+            # Request and validate the desired length inputs
+            desiredLengthInputMessage = "Please enter the desired lengths of chocolate in the form of {desiredAmount1,desiredAmount2,desiredAmountN}:"
+            desiredLengthInput = input(desiredLengthInputMessage)
+            desiredLengths = checkInputFormatting(desiredLengthInput)
 
         if desiredLengths == -1:
             continue
 
+
         print("Processing...")
+
 
         result = chocolateSolver.processLengths(barLengths, desiredLengths, is_verbose)
 
@@ -57,7 +67,7 @@ def main(is_verbose):
             print(f"Solved with {result} cuts made")
             print()
 
-
+        input("Press any key to continue")
 
 
 
